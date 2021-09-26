dc98b1310e061df6fd999f8edb0798fb5c7c274d
Hannah Lee <hannahsylee@gmail.com>
2021-09-26 13:10:34 -0700

    Add changes with using ES2015

---
+++ objectEnhancementsExercise.js
@@ -0,0 +1,85 @@
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+// }
+
+/* Write an ES2015 Version */
+function createInstructor(firstName, lastName){
+    return {
+        firstName,
+        lastName
+    }
+}
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+let favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+/* Write an ES2015 Version */
+var instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: 'That is my favorite!'
+}
+
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//         return "Hi!";
+//     },
+//     sayBye: function(){
+//         return this.firstName + " says bye!";
+//     }
+// }
+
+/* Write an ES2015 Version */
+var instructor = {
+    firstName: "Colt",
+    sayHi(){
+        return "Hi!";
+    },
+    sayBye(){
+        return this.firstName + " says bye!";
+    }
+}
+
+/* Write an ES2015 Version */
+function createAnimal(species, verb, noise){
+    return {
+		species,
+        [verb](){
+            return noise;
+        }
+		// bark(){
+        //     return [noise];
+        // },
+        // verb,
+        // noise 
+	};
+}
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+console.log(d.bark())  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+console.log(s.bleet()) //"BAAAAaaaa"
+
+
+
+
+
